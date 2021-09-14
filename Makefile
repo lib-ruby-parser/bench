364c503201b0a52abf54dc8a9efb8d8443841a4d
Ilya Bylich <ibylich@gmail.com>
2021-09-14 14:58:23 +0300

    added rust runner of lib-ruby-parser

--- Makefile
+++ Makefile
@@ -14,5 +14,10 @@ repos.zip: repos stats filelist
 	zip -r -q repos.zip repos/ stats filelist
 CLEAN += repos.zip
 
+RUST_DIR = rust-parse
+rust-parser: $(RUST_DIR)/src/main.rs $(RUST_DIR)/Cargo.toml
+	cargo build --release $(CARGOFLAGS) --manifest-path $(RUST_DIR)/Cargo.toml
+	cp $(RUST_DIR)/target/release/rust-parse rust-parser
+
 clean:
 	rm -rf $(CLEAN)
---
+++ rust-parse/src/main.rs
@@ -0,0 +1,57 @@
+#[cfg(feature = "jemallocator")]
+extern crate jemallocator;
+extern crate lib_ruby_parser;
+#[cfg(feature = "jemallocator")]
+#[global_allocator]
+static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+use lib_ruby_parser::{debug_level, Parser, ParserOptions};
+use std::time::Instant;
+
+fn files_to_parse() -> Vec<FileToParse> {
+    let filelist_path = std::env::var("FILELIST_PATH").unwrap();
+    let filelist = std::fs::read_to_string(&filelist_path).unwrap();
+
+    filelist
+        .lines()
+        .map(|filepath| FileToParse::new(filepath))
+        .collect::<Vec<_>>()
+}
+
+struct FileToParse {
+    filepath: String,
+    content: Vec<u8>,
+}
+
+impl FileToParse {
+    fn new(filepath: &str) -> Self {
+        Self {
+            filepath: filepath.to_owned(),
+            content: std::fs::read(filepath).unwrap(),
+        }
+    }
+
+    fn parse(self) {
+        let Self { filepath, content } = self;
+        let options = ParserOptions {
+            buffer_name: filepath,
+            debug: debug_level::NONE,
+            decoder: None,
+            token_rewriter: None,
+            record_tokens: false,
+        };
+        Parser::new(content, options).do_parse();
+    }
+}
+
+fn main() {
+    let files_to_parse = files_to_parse();
+
+    let start = Instant::now();
+    for file_to_parse in files_to_parse {
+        file_to_parse.parse();
+    }
+    let end = Instant::now();
+    let diff = (end - start).as_secs_f64();
+    println!("Time taken: {:.10}", diff)
+}
