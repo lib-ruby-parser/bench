4c6cb23e2063d6e8afff90bda3dcd842b74420b3
Ilya Bylich <ibylich@gmail.com>
2021-09-15 18:30:24 +0300

    expiriment with shared actions

---
+++ ruby-parser.rb
@@ -0,0 +1,16 @@
+filelist = File.read(ENV['FILELIST_PATH']).split("\n")
+files = filelist.map { |filepath| File.read(filepath) }
+require 'ripper'
+
+GC.disable
+
+def now
+    Process.clock_gettime(Process::CLOCK_MONOTONIC)
+end
+
+start = now
+files.each do |file|
+    Ripper.sexp(file)
+end
+
+puts "Time taken: #{now - start}"
